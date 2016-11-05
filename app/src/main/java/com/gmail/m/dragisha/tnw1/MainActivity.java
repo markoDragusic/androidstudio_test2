f843d4bbfcb3887ec1b744bc80e3437942450c7a
marko d <dragisha.m@gmail.com>
2016-11-05 19:46:07 +0100

    Initial commit

---
+++ app/src/main/java/com/gmail/m/dragisha/tnw1/MainActivity.java
@@ -0,0 +1,13 @@
+package com.gmail.m.dragisha.tnw1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+//TODO todor
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
